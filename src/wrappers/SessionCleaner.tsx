aeb20f2bb6c5299ff61dfe6f2e19e0665fa3bea1
Diwas Rimal <rimaldiwas10@gmail.com>
2024-10-04 19:37:23 +0545

    Fix sessionStorage not being cleaned on refresh

--- src/wrappers/SessionCleaner.tsx
+++ src/wrappers/SessionCleaner.tsx
@@ -2,9 +2,16 @@ import { PropsWithChildren, useEffect } from "react";
 
 export default function SessionCleaner({ children }: PropsWithChildren) {
   useEffect(() => {
-    return () => {
-      console.log("<SessionCleaner /> unmounted!");
-      sessionStorage.clear();
+    window.onbeforeunload = function () {
+      sessionStorage.setItem("refresh-initiator", window.location.href);
+    };
+    window.onload = function () {
+      if (
+        window.location.href === sessionStorage.getItem("refresh-initiator")
+      ) {
+        console.log("Cleaning sessionStorage..");
+        sessionStorage.clear();
+      }
     };
   }, []);
   return <>{children}</>;
